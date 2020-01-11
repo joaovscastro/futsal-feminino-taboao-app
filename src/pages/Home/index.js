4d0e2b64004a566e24806b241db6f19695bf87d4
João Castro <joaocastrojv@gmail.com>
2020-01-11 17:57:59 -0300

    Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,8 +3,18 @@ import { SafeAreaView, Text, View, Image } from 'react-native';
 
 import { Header, HeaderTexts, HeaderTextName, HeaderTextDesc, AvatarContent, Avatar,
   Container, Title, Destaque, DestaqueBg, DestaqueTitle, MelhoresMomentos,
-  MelhoresMomentosItem, MelhoresMomentosImg, MelhoresMomentosTitle } from './styles';
-import fotoAvatar  from "../../../assets/img/avatar.png"
+  MelhoresMomentosItem, MelhoresMomentosImg, MelhoresMomentosTitle,
+  UltimoJogo, JogoTitulo, JogoContent, JogoTimes, JogoInfo, TimeName,
+  JogoDetalhes, JogoDetalhesTitle, Elenco, ElencoFoto, ElencoName,
+  ElencoContent, Noticias, NoticiasImg, NoticiasTitle, NoticiasDesc,
+  JogoDetalhesView, JogoInfoTime, JogoInfoData, JogoLogo } from './styles';
+
+import fotoAvatar  from "../../../assets/img/avatar.png";
+import noticiaPlaceholder from "../../../assets/img/noticias-placeholder.jpg";
+import Brasao from "../../../assets/img/brasao.png";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+Icon.loadFont()
 
 export default function Home() {
   return (
@@ -27,19 +37,95 @@ export default function Home() {
         </Destaque>
         <Title>Melhores momentos</Title>
         <MelhoresMomentos horizontal={true} showsHorizontalScrollIndicator={false}>
-          <MelhoresMomentosItem>
+          <MelhoresMomentosItem underlayColor="#ffffff">
             <View>
               <MelhoresMomentosImg source={fotoAvatar} />
               <MelhoresMomentosTitle>Lorem</MelhoresMomentosTitle>
             </View>
           </MelhoresMomentosItem>
-          <MelhoresMomentosItem>
+          <MelhoresMomentosItem underlayColor="#ffffff">
             <View>
               <MelhoresMomentosImg source={fotoAvatar} />
               <MelhoresMomentosTitle>Lorem</MelhoresMomentosTitle>
             </View>
           </MelhoresMomentosItem>
         </MelhoresMomentos>
+
+        <UltimoJogo style={{
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 1,
+          },
+          shadowOpacity: 0.20,
+          shadowRadius: 1.41,
+          elevation: 2,
+        }}>
+          <JogoTitulo>Campeonato Paulista</JogoTitulo>
+          <JogoContent>
+            <JogoTimes>
+              <JogoLogo source={Brasao} resizeMode="contain" />
+              <TimeName>Taboao</TimeName>
+            </JogoTimes>
+            <JogoInfo>
+              <JogoInfoTime>19:30</JogoInfoTime>
+              <JogoInfoData>01/10/2020</JogoInfoData>
+            </JogoInfo>
+            <JogoTimes>
+            <JogoLogo source={Brasao} resizeMode="contain" />
+              <TimeName>Leoas da serra </TimeName>
+            </JogoTimes>
+          </JogoContent>
+          <JogoDetalhesView>
+          <JogoDetalhes underlayColor="#f3f3f3">
+              <JogoDetalhesTitle>Ver detalhes</JogoDetalhesTitle>
+            </JogoDetalhes>
+          </JogoDetalhesView>
+        </UltimoJogo>
+        <Title>Elenco</Title>
+        <Elenco horizontal={true} showsHorizontalScrollIndicator={false}>
+          <ElencoContent>
+            <ElencoFoto source={fotoAvatar} />
+            <ElencoName>Marielle</ElencoName>
+          </ElencoContent>
+          <ElencoContent>
+            <ElencoFoto source={fotoAvatar} />
+            <ElencoName>Marielle</ElencoName>
+          </ElencoContent>
+          <ElencoContent>
+            <ElencoFoto source={fotoAvatar} />
+            <ElencoName>Marielle</ElencoName>
+          </ElencoContent>
+          <ElencoContent>
+            <ElencoFoto source={fotoAvatar} />
+            <ElencoName>Marielle</ElencoName>
+          </ElencoContent>
+          <ElencoContent>
+            <ElencoFoto source={fotoAvatar} />
+            <ElencoName>Marielle</ElencoName>
+          </ElencoContent>
+          </Elenco>
+          <Title>Notícias</Title>
+          <Noticias underlayColor="#ffffff">
+            <NoticiasImg source={noticiaPlaceholder} />
+            <View style={{ flex: 1 }}>
+              <NoticiasTitle>Lorem ipsum in dolor</NoticiasTitle>
+              <NoticiasDesc>Lorem ipsum in dolor</NoticiasDesc>
+            </View>
+            <View>
+              <Icon name="chevron-right" size={30} color="#C4C4C4" />
+            </View>
+          </Noticias>
+          <Noticias underlayColor="#ffffff">
+            <NoticiasImg source={noticiaPlaceholder} />
+            <View style={{ flex: 1 }}>
+              <NoticiasTitle>Lorem ipsum in dolor</NoticiasTitle>
+              <NoticiasDesc>Lorem ipsum in dolor</NoticiasDesc>
+            </View>
+            <View>
+              <Icon name="chevron-right" size={30} color="#C4C4C4" />
+            </View>
+          </Noticias>
       </Container>
     </SafeAreaView>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -25,7 +25,7 @@ font-family: 'Axiforma';
 font-weight: 600;
 font-size: 12px;
 color: #fff;
-
+margin-top: -7px;
 `;
 
 export const AvatarContent = styled.View`
@@ -39,7 +39,7 @@ export const Avatar = styled.Image`
 `;
 
 export const Container = styled.ScrollView`
-background-color: #ffffff;
+background-color: #f3f3f3;
 border-top-left-radius: 25px;
 border-top-right-radius: 25px;
 padding: 0 20px;
@@ -56,6 +56,7 @@ margin: 30px 0 15px 0;
 export const Destaque = styled(RectButton)`
 border-radius: 8px;
 box-shadow: 0px -40px 10px rgba(4, 4, 4, 0.6);
+
 `;
 
 export const DestaqueBg = styled.ImageBackground`
@@ -63,7 +64,6 @@ height: 133px;
 border-radius: 8px;
 justify-content: flex-end;
 padding: 10px 15px;
-
 `;
 
 export const DestaqueTitle = styled.Text`
@@ -90,7 +90,9 @@ height: 115px;
 border-radius: 8px;
 `;
 
-export const MelhoresMomentosTitle = styled.Text`
+export const MelhoresMomentosTitle = styled.Text.attrs({
+  numberOfLines: 1,
+})`
 font-family: 'Axiforma';
 font-weight: 500;
 font-size: 14px;
@@ -98,5 +100,145 @@ color: #000000;
 margin: 10px 0;
 `;
 
+export const UltimoJogo = styled.View`
+background-color: #ffffff;
+margin: 30px 0 0 0;
+border-radius: 8px;
+`;
+
+export const JogoTitulo = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+text-align: center;
+font-family: 'Axiforma';
+font-weight: 500;
+font-size: 16px;
+color: #000000;
+margin: 15px 3px 0 3px;
+`;
+
+export const JogoContent = styled.View`
+flex-direction: row;
+margin: 15px 0 0 0;
+
+`;
+
+export const JogoTimes = styled.View`
+flex: 1;
+align-items: center;
+justify-content: center;
+`;
+
+export const JogoLogo = styled.Image`
+width: 70px;
+height: 70px;
+`;
+
+export const TimeName = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+font-family: 'Axiforma';
+font-weight: normal;
+font-size: 12px;
+color: #000000;
+margin: 10px 3px 3px 3px;
+text-align: center;
+`;
+
+export const JogoInfo = styled.View`
+flex: 1;
+align-items: center;
+justify-content: center;
+`;
+
+export const JogoInfoTime = styled.Text`
+font-family: 'Axiforma';
+font-weight: bold;
+font-size: 20px;
+color: #000000;
+`;
+
+export const JogoInfoData = styled.Text`
+font-family: 'Axiforma';
+font-weight: normal;
+font-size: 12px;
+color: #000000;
+margin-top: -8px;
+`;
+
+export const JogoDetalhesView = styled.View`
+border-top-width: 3px;
+border-top-color: #f3f3f3;
+margin-top: 10px;
+
+`;
+
+export const JogoDetalhes = styled(RectButton)`
+flex: 1;
+align-items: center;
+justify-content: center;
+padding: 15px 0 5px 0;
+
+`;
+
+export const JogoDetalhesTitle = styled.Text`
+font-family: 'Axiforma';
+font-weight: normal;
+font-size: 11px;
+color: #000000;
+text-transform: uppercase;
+letter-spacing: 1px;
+`;
+
+export const Elenco = styled.ScrollView`
+
+`;
+
+export const ElencoContent = styled.View`
+margin: 0 16px 0 0;
+`;
+
+export const ElencoFoto = styled.Image`
+width: 80px;
+height: 80px;
+`;
+
+export const ElencoName = styled.Text.attrs({
+  numberOfLines: 1,
+})`
+font-family: 'Axiforma';
+font-weight: normal;
+font-size: 11px;
+color: #000000;
+text-align: center;
+margin: 5px 0 0 0;
+`;
+
+export const Noticias = styled(RectButton)`
+flex-direction: row;
+align-items: center;
+margin: 0 0 20px 0;
+`;
+
+export const NoticiasImg = styled.Image`
+width: 100px;
+height: 100px;
+border-radius: 8px;
+margin: 0 15px 0 0;
+`;
+
+export const NoticiasTitle = styled.Text`
+font-family: 'Axiforma';
+font-weight: bold;
+font-size: 15px;
+color: #000000;
+`;
+
+export const NoticiasDesc = styled.Text`
+font-family: 'Axiforma';
+font-weight: normal;
+font-size: 13px;
+color: #A8A8A8;
+`;
 
 
