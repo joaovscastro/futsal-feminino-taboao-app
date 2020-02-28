fae64c3eaf57b42e50de35650f22b70edcc10ec6
João Castro <joaocastrojv@gmail.com>
2020-02-28 11:40:05 -0300

    RN Config

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  assets: ['./assets/fonts/']
+  assets: ['./assets/fonts/'],
+  dependencies: {
+    'react-native-onesignal': {
+      platforms: {
+        android: null,
+        // ios: null,
+      },
+    },
+  },
 };
